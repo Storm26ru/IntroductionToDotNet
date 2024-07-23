8f79d3c45c090da67a8dcabb32cb9edb8bdc86df
Storm <Storm@STORMSSD>
2024-07-24 00:33:56 +0300

    Position console

---
+++ Shooter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shooter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.SetWindowSize(100,40);
+			
+
+			int height =Console.WindowHeight;
+			int width = Console.WindowWidth;
+			Console.WriteLine($"Параметры окна {height},{width}");
+		}
+	}
+}
